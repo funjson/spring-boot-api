16fbad513439b936f7d126f2cfa2ff634a6b56c5
fjs <820621047@qq.com>
2021-03-27 14:23:18 +0800

    Initial commit

---
+++ euraka-server/src/test/java/cn/funjson/eurakaserver/EurakaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.funjson.eurakaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurakaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
